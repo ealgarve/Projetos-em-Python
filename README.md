45a866991d03180f9df0b8acf3311f86d2d8df84
ealgarve <ealgarve@hotmail.com>
2020-09-01 11:24:33 -0300

    copiei e colei o código
    Copiei e colei o exercicio do curso de Pytho, que ainda não esta funcionando direito.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Projetos em Python
- Repositório destinado a pratica de Python, para futuramente programar para microcontroladores, começando com o Arduino.
+    Repositório destinado a prática de Python, para futuramente programar para microcontroladores, começando com o Arduino.
+
+# PROJETOS
+
+#   Primos
+    num_primos: ainda não esta funcionando corretamente. 
+    diretrizes do projeto: estou tentando criar este código sem ver outro codigo pronto.
+
---
+++ num_primos/num_primos.py
@@ -0,0 +1,24 @@
+'''Exercício Python 52: Faça um programa que
+leia um número inteiro e diga se ele é ou não um número primo.
+'''
+print('Lista de números primos:')
+lista_primo = [1, 2, 3]
+proximo = 1
+while len(lista_primo) <= 19: # cria lista de 20 numeros 
+    lista_primo.append(lista_primo[len(lista_primo) - 1] + proximo) # adiciona o proximo número na lista
+    print('lista de primos: {}' .format(lista_primo))
+    print('último termo adicionado é {}' .format(lista_primo[len(lista_primo) - 1]))
+    conta = 0
+    for i in range(lista_primo[len(lista_primo) - 2], lista_primo[0], -1): # testa o novo número
+        if lista_primo[len(lista_primo) - 1] % lista_primo[i]  == 0: # (termo adicionado)/(demais termo)= resto 0
+            conta += 1 # conta quantas divisões = 0
+    if conta <= 1: # se o numero novo tem só um divisor, ele é primo e fica na lista
+        print('o número {} é primo' .format(i))
+        proximo += 1  # para incrementar o próximo número
+    else:
+        print('o número {} não é primo' .format(i))
+        lista_primo.remove(lista_primo[len(lista_primo) - 1]) # se o novo número não é primo, ele cai fora
+        proximo += 1
+    pass
+    
+print('lista de primos: {}' .format(lista_primo))
