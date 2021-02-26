8aa73a2eb9998339ac9b50e98fff427a18041056
ealgarve <ealgarve@hotmail.com>
2021-02-26 11:49:18 -0300

    Update testes.py

--- aprofundando/testes.py
+++ aprofundando/testes.py
@@ -18,10 +18,15 @@ class quatro_operacoes:
     def divisao(self):
         return self.a / self.b
 
+    def mostra(self):
+        return f'os valores são A = {self.a} e B = {self.b}'
+
 c, d = 2, 7
-print(f'--> {c} + {d} = {quatro_operacoes(c, d).soma():>6.2f}')
-print(f'--> {c} - {d} = {quatro_operacoes(c, d).subtracao():>6.2f}')
+numeros = quatro_operacoes(c, d)
+print(f'--> {c} + {d} = {numeros.soma():>6.2f}')
+print(f'--> {c} - {d} = {numeros.subtracao():>6.2f}')
 print(f'--> {c} x {d} = {quatro_operacoes(c, d).multi():>6.2f}')
 print(f'--> {c} / {d} = {quatro_operacoes(c, d).divisao():>6.2f}')
 print(sqrt(6**2 + 7**2))
 print(4**2)
+print(f'objeto "numeros", criado pela da classe "quatro_operacoes": {numeros.mostra()} ')
