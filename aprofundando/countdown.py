411d7b415c9785ac29aefd3fc53104c6c54cb9bb
ealgarve <ealgarve@hotmail.com>
2021-03-25 10:41:53 -0300

    countdown

---
+++ aprofundando/countdown.py
@@ -0,0 +1,20 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        hour, mins = divmod(mins, 60)
+        timer = f'{hour:0>2d}:{mins:0>2d}:{secs:0>2d}'
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+            
+    if t == 0:
+        print(f'00:00:00')
+    else:
+        print(f'{hour:0>2d}:{mins:0>2d}:{secs:0>2d}')
+    print('Fim do Tempo')
+
+t = input('Entre com o tempo em segundos: ')
+
+countdown(int(t))
